6b8737f6912a379e5ad045836cff50b74a2fc3ea
JaYoon Oh <dhwkdbs3@gmail.com>
2026-05-20 10:28:41 +0900

    [Passed] 굉장한 수 (123ms, 10MB)

---
+++ trail1/굉장한 수/amazing-number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        boolean amazingNumber;
+
+        if (a % 2 == 1 && a % 3 == 0 || (a % 2 == 0 && a % 5 == 0)) {
+            amazingNumber = true;
+        } else {
+            amazingNumber = false;
+        }
+
+        if (amazingNumber) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
